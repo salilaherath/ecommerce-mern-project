ac615d41a414fb9638999a387f3b479bebe5cc7e
salilaherath <salilaherath@gmail.com>
2023-02-16 17:50:48 +0530

    Login link in header

--- frontend/src/components/grayHeader/GrayHeader.jsx
+++ frontend/src/components/grayHeader/GrayHeader.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './grayHeader.scss';
 
 const GrayHeader = () => {
@@ -7,7 +8,13 @@ const GrayHeader = () => {
 				<span>For Inquiries: </span>+94 76 543 4789
 			</div>
 			<div className="right">
-				<p className="login">Login</p>
+				<Link
+					to={'/login'}
+					className="login"
+					style={{ textDecoration: 'none', color: 'black' }}
+				>
+					Login
+				</Link>
 
 				<div class="vl"></div>
 				<p className="currency">LKR</p>
