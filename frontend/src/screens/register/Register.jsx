1df47fcd71ee7b08a9863a25785b65de8da1bb64
salilaherath <salilaherath@gmail.com>
2023-02-16 16:10:13 +0530

    Register page

---
+++ frontend/src/screens/register/Register.jsx
@@ -0,0 +1,20 @@
+import './register.scss';
+
+const Register = () => {
+	return (
+		<div className="overlay">
+			<div className="container">
+				<h2>Register</h2>
+				<p>Name</p>
+				<input type="text" />
+				<p>Email</p>
+				<input type="text" />
+				<p>Password</p>
+				<input type="password" name="" id="" />
+				<button>Register</button>
+			</div>
+		</div>
+	);
+};
+
+export default Register;
